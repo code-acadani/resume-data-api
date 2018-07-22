9cbc75c6d0e02220989efc4f39d2f24dc0e22fed
gerry <gmercado6@gmail.com>
2018-07-22 14:03:14 -0700

    added all routes with api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,34 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
+ 
+ namespace :api do 
+  get "/students" => "students#index"
+  get "/students/:id" => "students#show"
+  patch "/students/:id" => "students#update"
+  delete "/students/:id" => "students#delete"
+
+  get "/skills" => "skills#index"
+  get "/skills/:id" "skills#show"
+  post "/skills" => "skills#create"
+  patch "/skills/:id" => "skills#update"
+  delete "/skills/:id" => "skills#delete"
+
+  get "/experiences" => "experiences#index"
+  get "/experiences/:id" => "experiences#show"
+  post "/experiences" => "experiences#create"
+  patch "/experiences" => "experiences#update"
+  delete "/experiences/:id" => "experiences#delete"
+
+  get "/educations" => "educations#index"
+  get "/educations/:id" => "educations#show"
+  post "/educations" => "educations#create"
+  patch "/educations/:id" => "educations#update"
+  delete "/educations/:id" => "educations#delete"
+
+  get "/capstones" => "capstones#index"
+  get "/capstones/:id" => "capstones#show"
+  post "/capstones" => "capstones#create"
+  patch "/capstones/:id" => "capstones#update"
+  delete "/capstones/:id" => "capstones#delete"
+
+ end 
 end
