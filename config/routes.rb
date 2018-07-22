09ee0ba5f479688c8fcb37b4fdc2efb75ffb397b
Asher Millhollin <ashermillhollin@gmail.com>
2018-07-22 14:07:52 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,31 +4,31 @@ Rails.application.routes.draw do
   get "/students" => "students#index"
   get "/students/:id" => "students#show"
   patch "/students/:id" => "students#update"
-  delete "/students/:id" => "students#delete"
+  delete "/students/:id" => "students#destroy"
 
   get "/skills" => "skills#index"
   get "/skills/:id" "skills#show"
   post "/skills" => "skills#create"
   patch "/skills/:id" => "skills#update"
-  delete "/skills/:id" => "skills#delete"
+  delete "/skills/:id" => "skills#destroy"
 
   get "/experiences" => "experiences#index"
   get "/experiences/:id" => "experiences#show"
   post "/experiences" => "experiences#create"
   patch "/experiences" => "experiences#update"
-  delete "/experiences/:id" => "experiences#delete"
+  delete "/experiences/:id" => "experiences#destroy"
 
   get "/educations" => "educations#index"
   get "/educations/:id" => "educations#show"
   post "/educations" => "educations#create"
   patch "/educations/:id" => "educations#update"
-  delete "/educations/:id" => "educations#delete"
+  delete "/educations/:id" => "educations#destroy"
 
   get "/capstones" => "capstones#index"
   get "/capstones/:id" => "capstones#show"
   post "/capstones" => "capstones#create"
   patch "/capstones/:id" => "capstones#update"
-  delete "/capstones/:id" => "capstones#delete"
+  delete "/capstones/:id" => "capstones#destroy"
 
  end 
 end
