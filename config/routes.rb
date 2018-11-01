b292b0a0d6a89542a07f0e442da32df9280e7214
danizaghian <dani.zaghian@gmail.com>
2018-11-01 10:12:54 -0700

    fixes routes and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,9 @@
 Rails.application.routes.draw do
- 
  namespace :api do 
-  get "/students" => "students#index"
-  get "/students/:id" => "students#show"
-  patch "/students/:id" => "students#update"
-  delete "/students/:id" => "students#destroy"
-
-  get "/skills" => "skills#index"
-  get "/skills/:id" "skills#show"
-  post "/skills" => "skills#create"
-  patch "/skills/:id" => "skills#update"
-  delete "/skills/:id" => "skills#destroy"
-
-  get "/experiences" => "experiences#index"
-  get "/experiences/:id" => "experiences#show"
-  post "/experiences" => "experiences#create"
-  patch "/experiences" => "experiences#update"
-  delete "/experiences/:id" => "experiences#destroy"
-
-  get "/educations" => "educations#index"
-  get "/educations/:id" => "educations#show"
-  post "/educations" => "educations#create"
-  patch "/educations/:id" => "educations#update"
-  delete "/educations/:id" => "educations#destroy"
-
-  get "/capstones" => "capstones#index"
-  get "/capstones/:id" => "capstones#show"
-  post "/capstones" => "capstones#create"
-  patch "/capstones/:id" => "capstones#update"
-  delete "/capstones/:id" => "capstones#destroy"
-
+  resources :students
+  resources :skills
+  resources :experiences
+  resources :educations
+  resources :capstones
  end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-50.times do 
+20.times do 
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
   Student.create(
-    first_name: Faker::Name.first_name, 
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email, 
+    first_name: first_name, 
+    last_name: last_name,
+    email: "#{first_name.downcase}.#{last_name.downcase}@gmail.com", 
     phone_number: Faker::PhoneNumber.cell_phone, 
     short_bio: Faker::PrincessBride.quote, 
     linkedIn_url: Faker::SiliconValley.url, 
@@ -22,9 +24,9 @@
     )
 end 
 
-150.times do
+50.times do
   Education.create(
-    student_id: rand(1...50),
+    student_id: rand(1...20),
     start_date: Faker::Date.backward(800),
     end_date: Faker::Date.backward(400),
     degree:Faker::Job.education_level,
@@ -33,9 +35,9 @@ end
     )
 end 
 
-150.times do
+100.times do
   Experience.create(
-    student_id: rand(1...50),
+    student_id: rand(1...20),
     start_date: Faker::Date.backward(800),
     end_date: Faker::Date.backward(400),
     job_title:Faker::Job.position,
@@ -44,16 +46,16 @@ end
     )
 end 
 
-250.times do
+100.times do
   Skill.create(
-    student_id: rand(1...50),
+    student_id: rand(1...20),
     name: Faker::Job.key_skill
     )
 end 
 
-150.times do
+50.times do
   Capstone.create(
-    student_id: rand(1...50),
+    student_id: rand(1...20),
     name: Faker::HarryPotter.spell,
     description: Faker::FamousLastWords.last_words,
     url: Faker::Avatar.image,
